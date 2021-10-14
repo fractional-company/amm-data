b1d77dd19cfbba070aa94f9c096aacd762862fd1
cvetlicni <klemen@fractional.art>
2021-10-14 17:00:28 +0200

    add block timestamp parsing

--- src/providers/blocks/constants.js
+++ src/providers/blocks/constants.js
@@ -1,3 +1,4 @@
 export const ENDPOINTS = {
   1: "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks",
+  4: "https://api.thegraph.com/subgraphs/name/blocklytics/rinkeby-blocks",
 }
